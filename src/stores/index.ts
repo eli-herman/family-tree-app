7d6ee8ee8e8000e86ff11f7b8031cc1b2e4270b5
Eli Herman <eli.j.herman@Elis-MacBook-Pro.local>
2026-02-03 10:02:36 -0600

    feat(01-02): export new stores from index
    - Add useFamilyStore export
    - Add useUserStore export
    - Keep existing authStore, feedStore, subscriptionStore exports

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,5 @@
 export { useAuthStore } from './authStore';
+export { useFamilyStore } from './familyStore';
 export { useFeedStore } from './feedStore';
 export { useSubscriptionStore } from './subscriptionStore';
+export { useUserStore } from './userStore';
